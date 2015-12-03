3fda75049fdaf9aaef44d781bcba3d61f87977be
Joseph W Becher <jwbecher@drazisil.com>
2015-12-03 15:38:17 -0500

    Time to save

--- index.js
+++ index.js
@@ -1,97 +1,144 @@
 var https = require('https')
-var DomParser = require('dom-parser')
-var parser = new DomParser()
 
-// Rate limits
-// What as rate per hour?
-var ratePerHour = 120
-// In milliseconds
-var howOftenPerPage = 45000
+// // Rate limits
+// // What as rate per hour?
+// var ratePerHour = 120
+// // In milliseconds
+// var howOftenPerPage = 45000
+//
+// var pageTimerId
+//
+// // Results stats
+// var totalResults
+// var resultsPerPage
+// var totalPages
+// var estimatedHours
+// var estimatedDays
 
-var pageNumber = 1
-var pageTimerId
+var apiUrlGhOrgPath = '/orgs/mozilla/repos'
 
-// Results stats
-var totalResults
-var resultsPerPage
-var totalPages
-var estimatedHours
-var estimatedDays
+fetchFromGithub(apiUrlGhOrgPath, function cb_fetchOrgs (err, res) {
+  if (err) {
+    // Exit with error
+    console.log('ERROR: ' + err)
+    process.exit(1)
+  }
 
-// Run it
-fetchpage(pageNumber)
+  processOrgResults(res, process.exit)
+})
 
-function fetchpage (pageNumber) {
-  fetchGitSearchResultsByPage(pageNumber)
-  pageNumber++
-  // if (pageNumber >= totalPages) {
-  if (pageNumber === 2) {
-    clearInterval(pageTimerId)
-  } else {
-    pageTimerId = setInterval(fetchpage, howOftenPerPage, pageNumber)
+// ******************
+// Process the list of projects to find ones with open issues
+// Calls addition processing for projects that do
+// TODO: exports the complete list of projects in csv
+// ********************************************
+function processOrgResults (results, exit_code) {
+  // list of projects with open issues
+  var projectsWithIssues = []
+
+  // This holds all the data for the Mozilla org on GitHub
+  var orgData = results
+  var repoTotalCount = results.length
+
+  console.log('Mozilla has ' + repoTotalCount + ' repos on GitHub with the following stats:')
+
+  console.log('name,stars,forks,open_issues')
+  for (var repoCounter = 0; repoCounter < repoTotalCount; repoCounter++) {
+    var currentRepo = orgData[repoCounter]
+
+    // Todo: export as csv
+    console.log(currentRepo.name + ',' +
+      currentRepo.stargazers_count + ',' +
+      currentRepo.forks_count + ',' +
+      currentRepo.open_issues_count)
+
+    // Check if repo has open issues
+    if (currentRepo.open_issues_count > 0) {
+      projectsWithIssues.push(currentRepo)
+    }
+
+    if (projectsWithIssues.length > 0) {
+      projectsWithIssues.reverse()
+      processIssues(projectsWithIssues, process.exit)
+    } else {
+      // Nothing more to do, exit with success
+      exit_code(0)
+    }
   }
 }
 
-function fetchGitSearchResultsByPage (pageNumber) {
-  var url = 'https://github.com/search?l=YAML&p=' + pageNumber + '&q=%22circle.yml%22+in%3Apath+language%3AYAML+path%3A%2F+language%3AYAML&ref=advsearch&type=Code&utf8=%E2%9C%93'
+function processIssues (projectsWithIssues, exit_code) {
+  if (projectsWithIssues.length === 0) {
+    exit_code(0)
+  }
 
-  fetchGitSearchResults(url, function cb_fetch (data) {
-    var oDOM = parser.parseFromString(data, 'text/html')
-    // Lets get the total results count
-    var sortDataDom = oDOM.getElementsByClassName('sort-bar')[0].childNodes
-
-    // Get the results list
-    var results = oDOM.getElementsByClassName('code-list-item')
-
-    // If this is the first page do some basic setup
-    if (pageNumber === 1) {
-      // How many results?
-      if (isNaN(sortDataDom[3].textContent.trim().split(' ')[1].toString().replace(',', ''))) {
-        totalResults = sortDataDom[3].textContent.trim().split(' ')[2].toString().replace(',', '').trim()
-        console.log('There are ' + totalResults + ' results.')
-      } else {
-        totalResults = sortDataDom[3].textContent.trim().split(' ')[1].toString().replace(',', '').trim()
-        console.log('Search timed out, only able to get ' + totalResults + ' at this time')
+  for (var projectCounter = 0; projectCounter < projectsWithIssues.length;
+     projectCounter++) {
+    var currentProject = projectsWithIssues[projectCounter]
+    var projectIssueUrl = '/repos/mozilla/' + currentProject.name + '/issues'
+    fetchFromGithub(projectIssueUrl, function cb_fetchOrgs (err, res) {
+      if (err) {
+        // Exit with error
+        console.log('ERROR: ' + err)
         process.exit(1)
       }
 
-      // How many results per page?
-      resultsPerPage = results.length
-      console.log('There are ' + resultsPerPage + ' results per page')
+      var issuesList = res
+      var issueTotalCount = res.length
 
-      // So how many pages?
-      totalPages = Math.ceil(totalResults / resultsPerPage)
-      console.log('There are ' + totalPages + ' pages of results')
+      console.log('number,title,age,is_pull')
+      for (var issueCounter = 0; issueCounter < issueTotalCount; issueCounter++) {
+        var currentIssue = issuesList[issueCounter]
 
-      console.log('The rate limit is ' + ratePerHour + ' per hour')
+        // Todo: export as csv
+        var csvIssueLine = currentIssue.number + ','
 
-      // So how long will this take?
-      estimatedHours = totalPages / (ratePerHour)
-      console.log('With a little extra time to avoid hitting the cap, it is estimated it will take ' + estimatedHours + ' hour(s) to complete')
+        csvIssueLine = csvIssueLine + currentIssue.title + ','
 
-      if (estimatedHours > 24) {
-        estimatedDays = estimatedHours / 24
-        console.log("That's " + estimatedDays + ' days, in case you were wondering')
+        csvIssueLine = csvIssueLine + getAge(currentIssue.created_at) + ','
+
+        if (currentIssue.pull_request) {
+          csvIssueLine = csvIssueLine + 'true'
+        } else {
+          csvIssueLine = csvIssueLine + 'false'
+        }
+
+        console.log(csvIssueLine)
       }
-    }
 
-    for (var i = 0; i < resultsPerPage; i++) {
-      parseGitSearchResultItem(results[i])
-    }
-  })
+      // TODO: figure out best way to callback this
+      exit_code(0)
+    })
+  }
 }
 
-function parseGitSearchResultItem (nodeResultItem) {
-  console.log(
-    nodeResultItem.getElementsByClassName('title')[0].childNodes[1].getAttribute('href') +
-    ',' +
-    nodeResultItem.getElementsByClassName('language')[0].childNodes[0].textContent.trim()
-  )
+function getAge (created_date) {
+  //Set the two dates
+  created_date = new Date(created_date)
+  var today = new Date()
+
+  //Get 1 day in milliseconds
+  var one_day = (1000 * 60 * 60 * 24)
+
+  //Calculate difference btw the two dates, and convert to days
+  var ageDays = Math.ceil((today.getTime() - created_date.getTime()) / (one_day))
+  return ageDays
 }
 
-function fetchGitSearchResults (url, callback) {
+// ***************************
+// fetched from GitHub using API
+// Returns: JSON
+// ****************************
+function fetchFromGithub (path, callback) {
+  var options = {
+    host: 'api.github.com',
+    path: path,
+    headers: {
+      'User-Agent': 'drazisil'
+    }
+  }
   var body = ''
-  https.get(url, function (res) {
+  https.get(options, function (res) {
     // console.log('statusCode: ', res.statusCode)
     // console.log('headers: ', res.headers)
 
@@ -99,9 +146,9 @@ function fetchGitSearchResults (url, callback) {
       body = body + d
     })
     res.on('end', function () {
-      callback(body)
+      callback(null, JSON.parse(body))
     })
   }).on('error', function (e) {
-    console.error(e)
+    callback(e)
   })
 }
