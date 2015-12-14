e573e57c91618aeb363b843ea53e55552bad9439
gsilvapt <gustavosantaremsilva@gmail.com>
2015-12-14 02:43:28 +0000

    changes in markdown file, although still a lot to work

--- dash-zero-shiny/source_from_repo.R
+++ dash-zero-shiny/source_from_repo.R
@@ -4,9 +4,15 @@
 ## sources
 setwd("~/Documents/dashboard-zero/data")
 
-comments <- read.table("comments.csv", header = TRUE, sep =",", row.names = NULL)
+comments <- read.csv("comments.csv", header = TRUE, sep =",")
 issues <- read.csv("issues.csv", header = TRUE, sep =",", row.names = NULL)
 labels <- read.csv("labels.csv", header = TRUE, sep =",", row.names = NULL)
 members <- read.csv("members.csv", header = TRUE, sep =",", row.names = NULL)
 milestones <- read.csv("milestones.csv", header = TRUE, sep =",", row.names = NULL)
-repos <- read.table("repos.csv", header = TRUE, sep =",", row.names = NULL)
+#repos <- read.csv("repos.csv", header = TRUE, sep =",")
+
+## Treat every data format
+comments$updated_date <- as.Date(comments$updated_date)
+issues$created_date <- as.Date(issues$created_date)
+issues$updated_date <- as.Date(issues$updated_date)
+milestones$due_on <- as.Date(milestones$due_on)
